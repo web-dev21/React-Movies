d4c751a469192b12ad77d269b9a9d3a62857df19
Yusupov Shakhriyor <shakhyusupov21@gmail.com>
2023-02-23 04:11:09 +0200

    first commit

---
+++ src/layout/Header.jsx
@@ -0,0 +1,21 @@
+function Header() {
+  return (
+    <nav className="purple darken-4">
+      <div className="nav-wrapper">
+        <a href="#!" className="brand-logo">
+          R-Movies
+        </a>
+        <ul className="right hide-on-med-and-down">
+          <li>
+            <a href="!#">Repos</a>
+          </li>
+          <li>
+            <a href="!#">Repos</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export { Header };
